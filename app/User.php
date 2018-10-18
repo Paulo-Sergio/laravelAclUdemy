1ccb7e93adf5c1a1a8c40b910267bef439431c0a
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-10-18 19:27:21 -0300

    trabalhando com ACL no Laravel. Papeis e Permissões de usuarios

---
+++ app/User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function eAdmin()
+    {
+      return $this->id == 1;
+    }
+
+    public function carros()
+    {
+      return $this->belongsToMany(Carro::class);
+    }
+
+    public function chamados()
+    {
+      return $this->belongsToMany(Chamado::class);
+    }
+
+    public function papeis()
+    {
+      return $this->belongsToMany(Papel::class);
+    }
+
+    public function adicionaPapel($papel)
+    {
+      if (is_string($papel)) {
+        $papel = Papel::where('nome', '=', $papel)->get();
+      }
+
+      if ($this->existePapel($papel)) {
+        return;
+      }
+
+      return $this->papeis()->attach($papel);
+    }
+
+    public function existePapel($papel)
+    {
+      if (is_string($papel)) {
+        $papel = Papel::where('nome', '=', $papel)->firstOrFail();
+      }
+
+      return (boolean) $this->papeis()->find($papel->id);
+    }
+}
